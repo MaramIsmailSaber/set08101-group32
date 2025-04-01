7ebd588f63fcd070f5e2740d7f7a41a1f2887398
Y0rk13 <yorkie120705f@gmail.com>
2025-04-01 18:15:25 +0100

    Fixed Scrolling

--- weatherDir/autoScroll.js
+++ weatherDir/autoScroll.js
@@ -3,14 +3,20 @@ const scroll = document.getElementById("scroll");
 animate(scroll);
 
 function animate(element) {
-	let elementWidth =  element.offsetWidth;
-	let parentWidth =  element.parentElement.offsetWidth;
-	let flag = 0;
-	
-	set interval(()=> {
-		element.style.marginLeft = --flag + "px";
-		
-		if elementWidth == -flag) {
-			flag = parentWidth;
-		}
-	}, 10);
+    let elementWidth = element.offsetWidth;
+    let parentWidth = element.parentElement.offsetWidth;
+    let flag = 0;
+    
+    function step() {
+        element.style.marginLeft = -flag + "px"; // move the element left by 'flag' px
+        flag++; // increment the flag to scroll the element to the left
+
+        if (flag >= elementWidth) { // when the entire element has scrolled off the screen
+            flag = 0; // reset the flag to restart the scroll from the beginning
+        }
+        
+        requestAnimationFrame(step); // request the next frame
+    }
+    
+    requestAnimationFrame(step); // start the animation
+}
