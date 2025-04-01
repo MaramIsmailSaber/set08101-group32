f0d750eea44725fe103fed163e9ff24af97dc905
Y0rk13 <yorkie120705f@gmail.com>
2025-04-01 18:42:37 +0100

    design changes

--- weatherDir/autoScroll.js
+++ weatherDir/autoScroll.js
@@ -6,17 +6,18 @@ function animate(element) {
     let elementWidth = element.offsetWidth;
     let parentWidth = element.parentElement.offsetWidth;
     let flag = 0;
+    const speed = 3; // Adjust this value to control the speed of scrolling
     
     function step() {
         element.style.marginLeft = -flag + "px"; // move the element left by 'flag' px
-        flag++; // increment the flag to scroll the element to the left
+        flag += speed; // increment the flag faster by the 'speed' value
 
         if (flag >= elementWidth) { // when the entire element has scrolled off the screen
             flag = 0; // reset the flag to restart the scroll from the beginning
         }
-        
+
         requestAnimationFrame(step); // request the next frame
     }
-    
+
     requestAnimationFrame(step); // start the animation
 }
